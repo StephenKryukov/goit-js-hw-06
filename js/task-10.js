6a29d97905ddb6049040b08c89749a3b3229f777
Stepan Kryukov <stephenkryukov@gmail.com>
2022-06-29 22:56:30 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberOfBoxes = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const createBox = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+let boxSize = 20;
+function createBoxes() {
+  for (let i = 0; i < numberOfBoxes.value; i++) {
+    boxSize += 10;
+    let newColor = getRandomHexColor();
+
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.margin = "5px";
+    box.style.borderRadius = "2px";
+    box.style.backgroundColor = `${newColor}`;
+    createBox.append(box);
+  }
+}
+
+function destroyBoxes() {
+  createBox.innerHTML = "";
+  boxSize = 20;
+}
