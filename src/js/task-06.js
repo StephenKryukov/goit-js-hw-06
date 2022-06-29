6c1d332c0d10a3218788b8f03c12e5a9b5d220e2
Stepan Kryukov <stephenkryukov@gmail.com>
2022-06-29 14:21:32 +0300

    6-9 done

---
+++ src/js/task-06.js
@@ -0,0 +1,16 @@
+// Напиши скрипт, который при потере фокуса на инпуте (событие blur), проверяет его содержимое на правильное количество введённых символов.
+
+const input = document.querySelector("#validation-input");
+
+const checkInputValue = () => {
+  if (input.value.length === Number(input.getAttribute("data-length"))) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.add("invalid");
+  }
+};
+
+input.addEventListener("blur", checkInputValue);
+
+//console.log(Number(input.getAttribute("data-length")));
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+//Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+//const bodyRef = document.body;
+const buttonRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+const changeBodyColor = () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = bodyRef.style.backgroundColor;
+};
+
+buttonRef.addEventListener("click", changeBodyColor);
